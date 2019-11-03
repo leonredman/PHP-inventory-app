d54a61cc743ace8aa0ba983a4775a5609e230c5e
leonredman <leonproart@yahoo.com>
2019-11-03 02:45:07 -0700

    project update

--- create.php
+++ create.php
@@ -15,7 +15,6 @@
     $store_location = $_POST["store_location"];
     $price = $_POST["price"];
 
-
     try{
        $statement = $pdo->prepare(
          "INSERT INTO inventory (category, item, brand, type, unit, size, expiration_date, stock_qty,
@@ -36,21 +35,65 @@
      }
    }
   ?>
+
   <html>
     <head>
-
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
       <!-- Bootstrap CSS -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-      <!-- <link rel="stylesheet" href="./css/styles.css"> -->
-
-      <title>Culinary Closet Inventory Mgmt</title>
+      <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+      <link rel="stylesheet" href="./css/styles.css">
+      <title>Weable Inventory Managemt</title>
     </head>
+
     <body>
-      <form action="./create.php" method="POST">
+
+      <div class="d-flex flex-row">
+          <div class="col-2 p-3 mb-2 text-white text-center" id="create-aside">
+              <div class="logo">
+                <img src="./images/logo_trans2.png">
+                  <h5>WEABLE INVENTORY</h5>
+              </div>
+
+                <!-- menu -->
+                <div class="aside-menu">
+                  <h6>Dashboard</h6>
+                    <a href="./create.php" class="text-secondary">Create Inventory</a>
+                    <br><br>
+                    <a href="/" class="text-secondary">All Inventory</a>
+                    <br><br>
+                    <a href="./shopping_list.php" class="text-secondary">Shopping List</a>
+                    <br><br>
+                    <!-- <a href="./search.php" class="text-secondary">Search Products</a> -->
+                </div>
+                <!-- end menu -->
+          </div>
+
+              <div>
+                  <div class="menu-bar"></div>
+                  <br></br>
+
+                 <!-- search form -->
+                  <div class="search-form">
+                      <h4> Create Inventory</h4>
+                      <br> <br>
+                        <form action="search.php" method="get">
+                          <label>
+                            Search Products
+                              <input type="text" name="keyword" autocomplete="off">
+                          </label>
+
+                          <input type="submit" class="btn text-light" value="Submit">
+                        </form>
+                  </div>
+                      <br>
+                 <!-- search end -->
+                  <div class="row-seperator"></div>
+                <br><br>
+
+      <form class="form  ml-4 pl-4" action="./create.php" method="POST">
 
         <label for="category">category</label><br>
         <input type="text" name="category" value=""><br>
@@ -85,6 +128,12 @@
         <button type="submit">Save</button>
       </form>
 
+     </div>
+    </div>
+
+    </div>
+    </div>
+</div>
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
