b20d2ff9e7e3f57827450e43dc3273e7aab400f6
leonredman <leonproart@yahoo.com>
2019-11-27 22:18:56 -0700

    project update

--- create.php
+++ create.php
@@ -73,6 +73,46 @@
           </div>
 
         <div>
+
+          <nav class="navbar navbar-expand-lg navbar-light" id="navbar-responsive">
+            <a class="navbar-brand" href="/">Weable Inventory</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="./create.php">Create Inventory <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/">All Inventory</a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="./shopping_list.php">Shopping List</a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="/">Go Back</a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="./logout.php">Log Out</a>
+                </li>
+              </ul>
+
+            </div>
+          </nav>
+
+
+
+
+
+
+
+
+
                   <div class="menu-bar"></div>
                   <br></br>
 
@@ -179,7 +219,7 @@
             <br>
 
             <button class="create-btn mt-3" type="submit">Save</button>
-          
+
         </form>
         </div>
     </div>
