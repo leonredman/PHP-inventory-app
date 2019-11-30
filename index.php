e6a11b1f86b11d686e08f98324821700be4869ab
leonredman <leonproart@yahoo.com>
2019-11-29 22:41:10 -0700

    project update

--- index.php
+++ index.php
@@ -46,13 +46,12 @@
       echo "<h4 style='color: red;'>".$e->getMessage(). "</h4>";
     }
   }
-
   ?>
 
   <html>
     <head>
       <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
 
       <!-- Bootstrap sCSS -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
@@ -64,14 +63,13 @@
     </head>
 
     <body>
+      <div class="container-fluid">
+      <div class="row">
+      <div class="col-2" id="aside">
 
 
-
-
-
-
-  <div class="d-flex flex-row">
-        <div class="col-2 p-3 mb-2 text-white text-center" id="aside">
+  <!-- <div class="d-flex flex-row"> -->
+        <!-- <div class="col-2 p-3 mb-2 text-white text-center" id="aside"> -->
           <div class="logo">
             <img src="./images/logo_trans2.png">
               <h5>WEABLE INVENTORY</h5>
@@ -80,13 +78,7 @@
               <!-- menu -->
               <div class="aside-menu">
 
-
-
-
-
                 <h6>Dashboard</h6>
-
-
 
                 <a href="./create.php" class="text-secondary">Create Inventory</a>
                 <br>
@@ -103,9 +95,9 @@
               </div>
         </div>
 
-          <div>
+          <div class="col">
 
-            <nav class="navbar navbar-expand-lg navbar-light" id="navbar-responsive">
+            <nav class="navbar navbar-expand-lg navbar-light col-10" id="navbar-responsive">
               <a class="navbar-brand" href="/">Weable Inventory</a>
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -113,9 +105,11 @@
 
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="./create.php">Create Inventory <span class="sr-only">(current)</span></a>
+
+                  <li class="nav-item">
+                    <a class="nav-link" href="./create.php">Create Inventory</a>
                   </li>
+                  
                   <li class="nav-item">
                     <a class="nav-link" href="/">All Inventory</a>
                   </li>
@@ -135,17 +129,6 @@
 
               </div>
             </nav>
-
-
-
-
-
-
-
-
-
-
-
 
             <div class="menu-bar">
             </div>
@@ -179,8 +162,8 @@
 
               </div>
 
-              <div>
-            <table class="table table-hover">
+              <div class="container-fluid">
+            <table class="table table-hover" id="responsive-table">
               <tr>
                 <th>id</th>
                 <th>category</th>
@@ -222,8 +205,9 @@
             </table>
           </div>
           </div>
+        </div>
   </div>
-
+</div>
 
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
