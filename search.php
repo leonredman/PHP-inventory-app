5d0cb8bcd3b9f8bd1038ef5e224d17dad98a6f5c
leonredman <leonproart@yahoo.com>
2019-12-05 22:23:52 -0700

    project update

--- search.php
+++ search.php
@@ -42,43 +42,69 @@
    </head>
 
    <body>
+     <div class="container-fluid">
+       <div class="row">
+         <div class="col-2" id="aside">
 
-     <div class="d-flex flex-row">
-           <div class="col-2 p-3 mb-2 text-white text-center" id="aside">
-             <div class="logo">
-               <img src="./images/logo_trans2.png">
-                 <h5>WEABLE INVENTORY</h5>
-             </div>
+           <!-- <div class="d-flex flex-row">
+           <div class="col-2 p-3 mb-2 text-white text-center" id="aside"> -->
+            <div class="logo">
+              <img src="./images/logo_trans2.png">
+              <h5>WEABLE INVENTORY</h5>
+            </div>
+              <!-- menu -->
+              <div class="aside-menu">
+                <h6>Dashboard</h6>
 
-                 <!-- menu -->
-                 <div class="aside-menu">
+                <a href="./create.php" class="text-secondary">Create Inventory</a>
+                <br>
+                <br>
+                <a href="/" class="text-secondary">All Inventory</a>
+                <br>
+                <br>
+                <a href="./shopping_list.php" class="text-secondary">Shopping List</a>
+                <br>
+                <br>
+                <a href="logout.php">Logout</a>
+               <!-- end menu -->
+              </div>
+            </div>
 
+             <div class="col">
+               <nav class="navbar navbar-expand-lg navbar-light col-10" id="navbar-responsive">
+                 <a class="navbar-brand" href="/">Weable Inventory</a>
+                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                   <span class="navbar-toggler-icon"></span>
+                 </button>
 
+                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                   <ul class="navbar-nav mr-auto">
+                     <li class="nav-item">
+                       <a class="nav-link" href="./create.php">Create Inventory</a>
+                     </li>
+                     <li class="nav-item">
+                       <a class="nav-link" href="/">All Inventory</a>
+                     </li>
 
+                     <li class="nav-item">
+                       <a class="nav-link" href="./shopping_list.php">Shopping List</a>
+                     </li>
 
+                     <li class="nav-item">
+                       <a class="nav-link" href="/">Go Back</a>
+                     </li>
 
-                   <h6>Dashboard</h6>
+                     <li class="nav-item">
+                       <a class="nav-link" href="./logout.php">Log Out</a>
+                     </li>
+                   </ul>
 
+                 </div>
+               </nav>
 
-                   <a href="./create.php" class="text-secondary">Create Inventory</a>
-                   <br>
-                   <br>
-                   <a href="/" class="text-secondary">All Inventory</a>
-                   <br>
-                   <br>
-                   <a href="./shopping_list.php" class="text-secondary">Shopping List</a>
-                   <br>
-                   <br>
-   <!-- end menu -->
- </div>
-</div>
+               <div class="menu-bar">
 
-
-<div>
-  <div class="menu-bar">
-
-
-  </div>
+               </div>
   <br>
   <br>
 
@@ -94,12 +120,22 @@
    <br> <br>
  <!-- search form  end-->
 
+  <br>
+ <div class="pagination ml-5 mr-2 pl-3">
+   <p>Displaying Pages </p>
+   <div class="page-count">
+   <?php for($x = 1; $x <= $pages; $x++): ?>
+     <a href="?page=<?php echo $x; ?>&per-page=<?php echo $perPage; ?>"<?php if($page === $x) {echo ' class="selected"'; } ?>><?php echo $x; ?></a>
+   <?php endfor; ?>
+ </div>
+ </div>
+
  <div class="row-seperator">
 
  </div>
 
-
- <table class="table table-hover">
+<div class="container-fluid">
+ <table class="table table-hover" id="responsive-table">
 <tr>
   <th>id</th>
   <th>category</th>
@@ -139,10 +175,15 @@
 
   <?php } ?>
 </table>
+</div>
+</div>
+</div>
+</div>
+</div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-</body>
+    </body>
 </html>
