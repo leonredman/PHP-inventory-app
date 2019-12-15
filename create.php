e851a1604dc5bd9f16f9b8e80974175c70a06150
leonredman <leonproart@yahoo.com>
2019-12-15 10:34:25 -0700

    new menu layout update

--- create.php
+++ create.php
@@ -1,34 +1,44 @@
   <?php
 
+    session_start();
+
+    if(isset($_SESSION["username"]))
+  {
+    // echo '<h3>Login Success, Welcome - '.$_SESSION["username"].'</h3>';
+    // echo '<br /><br /><a href="/logout.php">Logout</a>';
+  }
+else {
+  {
+    header("location:./login.php");
+  }
+}
     require('database.php');
 
-   // create new user
-   if($_SERVER['REQUEST_METHOD'] == "POST") {
-    $category = $_POST["category"];
-    $item = $_POST["item"];
-    $brand = $_POST["brand"];
-    $type = $_POST["type"];
-    $unit = $_POST["unit"];
-    $size = $_POST["size"];
-    $expiration_date = $_POST["expiration_date"];
-    $stock_qty = $_POST["stock_qty"];
-    $store_location = $_POST["store_location"];
-    $price = $_POST["price"];
+   // create new item
 
-    try{
-       $statement = $pdo->prepare(
-         "INSERT INTO inventory (category, item, brand, type, unit, size, expiration_date, stock_qty,
-            store_location, price) VALUES (:category, :item, :brand, :type, :unit, :size, :expiration_date, :stock_qty, :store_location, :price);"
+     if($_SERVER['REQUEST_METHOD'] == "POST") {
+     $category = $_POST["category"];
+     $item = $_POST["item"];
+     $brand = $_POST["brand"];
+     $unit = $_POST["unit"];
+     $size = $_POST["size"];
+     $expiration_date = $_POST["expiration_date"];
+     $stock_qty = $_POST["stock_qty"];
+     $store_location = $_POST["store_location"];
+
+     try{
+        $statement = $pdo->prepare(
+          "INSERT INTO inventory3 (category, item, brand, unit, size, expiration_date, stock_qty,
+             store_location) VALUES (:category, :item, :brand, :unit, :size, :expiration_date, :stock_qty, :store_location);"
       );
 
-       $statement->execute(['category' => $category, 'item' => $item, 'brand' => $brand, 'type' => $type, 'unit' => $unit,
-        'size' => $size, 'expiration_date' => $expiration_date,
-        'stock_qty' => $stock_qty, 'store_location' => $store_location, 'price' => $price]);
-        echo "Insert Products: {$brand} {$type}";
+      $statement->execute(['category' => $category, 'item' => $item, 'brand' => $brand, 'unit' => $unit, 'size' => $size,
+       'expiration_date' => $expiration_date, 'stock_qty' => $stock_qty, 'store_location' => $store_location]);
+      echo " Product Successfully Added: {$item} {$brand}";
 
         $id = $pdo->lastInsertId();
 
-        echo "<script>location.href='./read.php?show=one&id={$id}'</script>";
+        // echo "<script>location.href='./read.php?show=one&id={$id}'</script>";
 
      } catch(PDOException $e){
       echo "<h4 style='color: red;'>".$e->getMessage(). "</h4>";
@@ -50,18 +60,51 @@
 
     <body>
       <div class="container-fluid">
-          <div class="row">
-        <div class="col-2" id="aside">
+
+        <div>
+                <nav class="navbar navbar-expand-lg navbar-light" id="navbar-responsive">
+                              <a class="navbar-brand" href="/">Weable Inventory</a>
+                              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                              </button>
+
+                              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav mr-auto">
+                                  <li class="nav-item">
+                                    <a class="nav-link" href="./create.php">Create Inventory <span class="sr-only">(current)</span></a>
+                                  </li>
+                                  <li class="nav-item">
+                                    <a class="nav-link" href="all_inventory.php">All Inventory</a>
+                                  </li>
+
+                                  <li class="nav-item">
+                                    <a class="nav-link" href="./shopping_list.php">Shopping List</a>
+                                  </li>
+
+                                  <li class="nav-item">
+                                    <a class="nav-link" href="/">Go Back</a>
+                                  </li>
+
+                                  <li class="nav-item">
+                                    <a class="nav-link" href="./logout.php">Log Out</a>
+                                  </li>
+                                </ul>
+
+                              </div>
+                            </nav>
+
+          <!-- <div class="row">
+        <div class="col-2" id="aside"> -->
 
       <!-- <div class="d-flex flex-row"> -->
           <!-- <div class="col-2 p-3 mb-2 text-white text-center" id="create-aside"> -->
-              <div class="logo">
+              <!-- <div class="logo">
                 <img src="./images/logo_trans2.png">
                   <h5>WEABLE INVENTORY</h5>
-              </div>
+              </div> -->
 
                 <!-- menu -->
-                <div class="aside-menu">
+                <!-- <div class="aside-menu">
                   <h6>Dashboard</h6>
                     <a href="./create.php" class="text-secondary">Create Inventory</a>
                     <br>
@@ -72,15 +115,15 @@
                     <a href="./shopping_list.php" class="text-secondary">Shopping List</a>
                     <br>
                     <br>
-                    <a href="/">Go Back</a>
+                    <a href="/">Go Back</a> -->
                     <!-- <a href="./search.php" class="text-secondary">Search Products</a> -->
                     <!-- end menu -->
-                </div>
+                <!-- </div>
           </div>
 
-        <div class="col">
+        <div class="col"> -->
 
-          <nav class="navbar navbar-expand-lg navbar-light col-10" id="navbar-responsive">
+          <!-- <nav class="navbar navbar-expand-lg navbar-light col-10" id="navbar-responsive">
             <a class="navbar-brand" href="/">Weable Inventory</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
@@ -109,7 +152,7 @@
               </ul>
 
             </div>
-          </nav>
+          </nav> -->
 
 
 
@@ -140,8 +183,10 @@
                  <!-- search end -->
                   <div class="row-seperator"></div>
                 <br><br>
-
-      <form class="form  ml-4 pl-4" action="./create.php" method="POST">
+    <div class="container" id="form-container">
+      <form class="form" action="./create.php" method="POST">
+        <div class="row">
+          <div class="form-group col-md-6">
 
         <!-- <label for="category">category</label><br>
         <input type="text" name="category" value=""><br> -->
@@ -163,72 +208,72 @@
           <option value="Snacks">Snacks</option>
           <option value="Veggies">Veggies</option>
         </select>
+      </div>
+    </div>
 
         <br>
         <br>
 
           <div class="row">
-              <div class="col-3">
+                <div class="form-group col-md-6">
                 <label for="item">Item</label><br>
-                <input type="text" name="item" value=""><br>
+                <input type="text" class="form-control" name="item" value=""><br>
               </div>
 
-              <div class="col-3">
+              <div class="form-group col-md-6">
                 <label for="brand">Brand</label><br>
-                <input type="text" name="brand" value=""><br>
+                <input type="text" class="form-control" name="brand" value=""><br>
               </div>
 
-              <div class="col-3">
+              <div class="form-group col-md-6">
                 <label for="type">Type</label><br>
-                <input type="text" name="type" value=""><br>
+                <input type="text" class="form-control" name="type" value=""><br>
               </div>
           </div>
 
             <br>
 
           <div class="row">
-              <div class="col-3">
+              <div class="form-group col-md-6">
                 <label for="unit">Unit</label><br>
-                <input type="text" name="unit" value=""><br>
+                <input type="text" class="form-control" name="unit" value=""><br>
               </div>
 
-              <div class="col-3">
+              <div class="form-group col-md-6">
                 <label for="size">Size</label><br>
-                <input type="text" name="size" value=""><br>
+                <input type="text"  class="form-control" name="size" value=""><br>
               </div>
 
-              <div class="col-3">
+              <div class="form-group col-md-6">
                 <label for="expiration_date">Expiration Date</label><br>
-                <input type="text" name="expiration_date" value=""><br>
+                <input type="text" class="form-control" name="expiration_date" value=""><br>
               </div>
           </div>
 
             <br>
 
           <div class="row">
-            <div class="col-3">
+            <div class="form-group col-md-6">
               <label for="stock_qty">Stock Qty</label><br>
-              <input type="text" name="stock_qty" value=""><br>
+              <input type="text" class="form-control" name="stock_qty" value=""><br>
             </div>
 
-            <div class="col-3">
+            <div class="form-group col-md-6">
               <label for="store_location">Store Location</label><br>
-              <input type="text" name="store_location" value=""><br>
+              <input type="text" class="form-control" name="store_location" value=""><br>
             </div>
 
             <br>
 
-            <div class="col-3">
+            <div class="form-group col-md-6">
               <label for="price">Price</label><br>
-              <input type="text" name="price" value=""><br>
+              <input type="text" class="form-control" name="price" value=""><br>
             </div>
           </div>
-            <br>
 
-            <button class="create-btn mt-3" type="submit">Save</button>
-
+            <button class="create-btn mt-5" type="submit">Save</button>
         </form>
-        </div>
+      </div>
     </div>
 
     </div>
