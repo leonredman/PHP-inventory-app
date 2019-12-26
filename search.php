2081fb07d9649cd452a1d97f58f31ea5a3b1ec4b
leonredman <leonproart@yahoo.com>
2019-12-25 22:17:28 -0700

    code cleanup

--- search.php
+++ search.php
@@ -43,44 +43,41 @@
 
    <body>
      <div class="container-fluid">
-    
-               <nav class="navbar navbar-expand-lg navbar-light" id="navbar-responsive">
-                 <a class="navbar-brand" href="/">Weable Inventory</a>
-                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                   <span class="navbar-toggler-icon"></span>
-                 </button>
 
-                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                   <ul class="navbar-nav mr-auto">
-                     <li class="nav-item">
-                       <a class="nav-link" href="./create.php">Create Inventory</a>
-                     </li>
-                     <li class="nav-item">
-                       <a class="nav-link" href="/">All Inventory</a>
-                     </li>
+       <nav class="navbar navbar-expand-lg navbar-light" id="navbar-responsive">
+         <a class="navbar-brand" href="/">Weable Inventory</a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+         </button>
 
-                     <li class="nav-item">
-                       <a class="nav-link" href="./shopping_list.php">Shopping List</a>
-                     </li>
+         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+           <ul class="navbar-nav mr-auto">
+             <li class="nav-item">
+               <a class="nav-link" href="./create.php">Create Inventory</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="/">All Inventory</a>
+             </li>
 
-                     <li class="nav-item">
-                       <a class="nav-link" href="/">Go Back</a>
-                     </li>
+             <li class="nav-item">
+               <a class="nav-link" href="./shopping_list.php">Shopping List</a>
+             </li>
 
-                     <li class="nav-item">
-                       <a class="nav-link" href="./logout.php">Log Out</a>
-                     </li>
-                   </ul>
+             <li class="nav-item">
+               <a class="nav-link" href="/">Go Back</a>
+             </li>
 
-                 </div>
-               </nav>
+             <li class="nav-item">
+               <a class="nav-link" href="./logout.php">Log Out</a>
+             </li>
+           </ul>
 
-               <div class="menu-bar">
+         </div>
+       </nav>
 
-               </div>
-  <br>
-  <br>
-
+     <div class="menu-bar"></div>
+      <br>
+      <br>
 
     <form action="search.php" method="get">
      <label>
@@ -91,70 +88,57 @@
      <input type="submit" value="Submit">
    </form>
    <br> <br>
- <!-- search form  end-->
+    <!-- search form  end-->
 
+    <div class="row-seperator"></div>
 
+      <div class="container-fluid">
+      <table class="table table-hover" id="responsive-table">
+      <tr>
+      <th>category</th>
+      <th>item</th>
+      <th>brand</th>
+      <th>unit</th>
+      <th>size</th>
+      <th>expiration_date</th>
+      <th>stock_qty</th>
+      <th>store_location</th>
+      <th>edit</th>
+      <th>delete</th>
+      </tr>
 
- <div class="row-seperator">
+      <?php foreach($results as $inventory) { ?>
+      <tr>
+        <td><?php echo $inventory->category; ?></td>
+        <td><?php echo $inventory->item; ?></td>
+        <td><?php echo $inventory->brand; ?></td>
+        <td><?php echo $inventory->unit; ?></td>
+        <td><?php echo $inventory->size; ?></td>
+        <td><?php echo $inventory->expiration_date; ?></td>
+        <td><?php echo $inventory->stock_qty; ?></td>
+        <td><?php echo $inventory->store_location; ?></td>
+        <td>
+          <a href="./update.php?id=<?php echo $inventory->id; ?>">edit</a>
+        </td>
+        <td>
+          <a href="./delete.php?id=<?php echo $inventory->id; ?>" onclick="confirm()">delete</a>
+        </td>
+      </tr>
 
- </div>
+    <?php } ?>
+    </table>
 
-<div class="container-fluid">
- <table class="table table-hover" id="responsive-table">
-<tr>
-  <!-- <th>id</th> -->
-  <th>category</th>
-  <th>item</th>
-  <th>brand</th>
-  <!-- <th>type</th> -->
-  <th>unit</th>
-  <th>size</th>
-  <th>expiration_date</th>
-  <th>stock_qty</th>
-  <th>store_location</th>
-  <!-- <th>price</th> -->
-  <th>edit</th>
-  <th>delete</th>
-</tr>
-
-<?php foreach($results as $inventory) { ?>
-  <tr>
-    <!-- <td><?php echo $inventory->id; ?></td> -->
-    <td><?php echo $inventory->category; ?></td>
-    <td><?php echo $inventory->item; ?></td>
-    <td><?php echo $inventory->brand; ?></td>
-    <!-- <td><?php echo $inventory->type; ?></td> -->
-    <td><?php echo $inventory->unit; ?></td>
-    <td><?php echo $inventory->size; ?></td>
-    <td><?php echo $inventory->expiration_date; ?></td>
-    <td><?php echo $inventory->stock_qty; ?></td>
-    <td><?php echo $inventory->store_location; ?></td>
-    <!-- <td><?php echo $inventory->price; ?></td> -->
-    <td>
-      <a href="./update.php?id=<?php echo $inventory->id; ?>">edit</a>
-    </td>
-    <td>
-      <a href="./delete.php?id=<?php echo $inventory->id; ?>" onclick="confirm()">delete</a>
-    </td>
-  </tr>
-
-  <?php } ?>
-</table>
-
-<br>
-<div class="pagination ml-5 mr-2 pl-3">
- <p>Displaying Pages </p>
- <div class="page-count">
- <?php for($x = 1; $x <= $pages; $x++): ?>
-   <a href="?page=<?php echo $x; ?>&per-page=<?php echo $perPage; ?>"<?php if($page === $x) {echo ' class="selected"'; } ?>><?php echo $x; ?></a>
- <?php endfor; ?>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
+    <br>
+        <div class="pagination ml-5 mr-2 pl-3">
+          <p>Displaying Pages </p>
+            <div class="page-count">
+            <?php for($x = 1; $x <= $pages; $x++): ?>
+             <a href="?page=<?php echo $x; ?>&per-page=<?php echo $perPage; ?>"<?php if($page === $x) {echo ' class="selected"'; } ?>><?php echo $x; ?></a>
+            <?php endfor; ?>
+            </div>
+        </div>
+    </div>
+  </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
