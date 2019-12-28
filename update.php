08c751da688592cbfd9c8f366cde9cb31aa0d397
leonredman <leonproart@yahoo.com>
2019-12-28 13:32:35 -0700

    code cleanup

--- update.php
+++ update.php
@@ -75,74 +75,58 @@
 
     <body>
       <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light" id="navbar-responsive">
+          <a class="navbar-brand" href="/">Weable Inventory</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item">
+                    <a class="nav-link" href="./create.php">Create Inventory</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">All Inventory</a>
+                  </li>
 
-                <!-- </div> -->
-                <!-- end menu -->
-          <!-- </div> -->
+                  <li class="nav-item">
+                    <a class="nav-link" href="./shopping_list.php">Shopping List</a>
+                  </li>
 
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">Go Back</a>
+                  </li>
 
-                  <!-- <div class="col"> -->
-                <nav class="navbar navbar-expand-lg navbar-light" id="navbar-responsive">
-                  <a class="navbar-brand" href="/">Weable Inventory</a>
-                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
+                  <li class="nav-item">
+                    <a class="nav-link" href="./logout.php">Log Out</a>
+                  </li>
+                </ul>
 
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                      <li class="nav-item">
-                        <a class="nav-link" href="./create.php">Create Inventory</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="/">All Inventory</a>
-                      </li>
+              </div>
+        </nav>
 
-                      <li class="nav-item">
-                        <a class="nav-link" href="./shopping_list.php">Shopping List</a>
-                      </li>
+          <div class="menu-bar"></div>
+            <br></br>
 
-                      <li class="nav-item">
-                        <a class="nav-link" href="/">Go Back</a>
-                      </li>
+              <!-- search form -->
+                <div class="search-form">
+                    <h4> Update Inventory</h4>
+                    <br> <br>
+                      <form action="search.php" method="get">
+                        <label>
+                          Search Products
+                            <input type="text"  class="form-control" name="keyword" autocomplete="off">
+                        </label>
 
-                      <li class="nav-item">
-                        <a class="nav-link" href="./logout.php">Log Out</a>
-                      </li>
-                    </ul>
+                        <input type="submit" class="btn text-light" value="Submit">
+                      </form>
+                </div>
+            <br>
+              <!-- search end -->
+            <br>
 
-                  </div>
-                </nav>
-
-                  <div class="menu-bar"></div>
-                  <br></br>
-
-                 <!-- search form -->
-                  <div class="search-form">
-                      <h4> Update Inventory</h4>
-                      <br> <br>
-                        <form action="search.php" method="get">
-                          <label>
-                            Search Products
-                              <input type="text"  class="form-control" name="keyword" autocomplete="off">
-                          </label>
-
-                          <input type="submit" class="btn text-light" value="Submit">
-                        </form>
-                  </div>
-                      <br>
-                 <!-- search end -->
-                 <br>
-                 <!-- <div class="pagination ml-5 mr-2 pl-3">
-                   <p>Displaying Pages </p>
-                   <div class="page-count">
-                   <?php for($x = 1; $x <= $pages; $x++): ?>
-                     <a href="?page=<?php echo $x; ?>&per-page=<?php echo $perPage; ?>"<?php if($page === $x) {echo ' class="selected"'; } ?>><?php echo $x; ?></a>
-                   <?php endfor; ?>
-                 </div>
-                 </div> -->
-
-                  <div class="row-seperator"></div>
-                <br><br>
+            <div class="row-seperator"></div>
+              <br><br>
 
                 <div class="container" id="update-container">
 
@@ -166,15 +150,9 @@
                             <label for="brand">Brand</label><br>
                             <input type="text" class="form-control" name="brand" value="<?php echo $results[0]->brand; ?>"><br>
                           </div>
-
-                          <!-- <div class="col-4">
-                            <label for="type">Type</label><br>
-                            <input type="text" name="type" value="<?php echo $results[0]->type; ?>"><br>
-                          </div> -->
                         </div>
 
-                        <br>
-
+                    <br>
                   <div class="row">
                       <div class="form-group col-md-4">
                         <label for="unit">Unit</label><br>
@@ -213,18 +191,11 @@
                   <button class="update-btn mt-5" type="submit">Save</button>
                 </form>
               </div>
-              </div>
             </div>
-      </div>
-    </div>
 
-                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-
-
-
-              </body>
-
+    </body>
   </html>
